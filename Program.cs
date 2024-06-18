d0f9ba63c9afcd229877ecfd369622b536dc7f9e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-18 20:32:27 -0300

    ProjetoFinalFeito

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Security.Cryptography;
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta; 
+Console.WriteLine("---Estacionamento---\n"); 
+Console.ResetColor();
+Console.WriteLine("---Tamanho do veículo---(P/G)");
+string digitado = Console.ReadLine()!.ToUpper();
+string veiculo=digitado; 
+
+if(digitado!="P"&&digitado!="G") 
+{
+    Console.WriteLine("Tamanho inválido.");
+    return;
+
+}
+else if(digitado=="P")
+{ 
+    digitado="25";
+
+}
+else if(digitado=="G")
+{
+    digitado="50";
+
+}
+
+decimal PrecoHora=Convert.ToDecimal(digitado);
+Console.WriteLine("---Tempo---(min)");
+Double Tempo = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("---Serviço Manobrista---(S/N)");
+digitado = Console.ReadLine()!.ToUpper();
+if(digitado!="S"&&digitado!="N")
+{
+    Console.WriteLine("Tempo inválido.");
+    return;
+
+}
+else if(digitado=="S")
+{
+    digitado="0,20";
+
+}
+else if(digitado=="N")
+{
+    digitado="1";
+    
+}
+
+decimal PrecoManobrista = Convert.ToDecimal(digitado);
+Console.WriteLine("---Serviço Lavagem---(S/N)");
+digitado = Console.ReadLine()!.ToUpper();
+
+if(digitado!="S"&&digitado!="N")
+{
+    return;
+    
+}
+else if(digitado=="S" && veiculo=="P")
+{
+    digitado="50";
+    
+} 
+else if(digitado=="S" && veiculo=="G")
+{
+    digitado="100";
+    
+}
+else
+{
+    digitado="0";
+    
+}
+
+Double PrecoLavagem = Convert.ToDouble(digitado);
+decimal PrecoFinalEstacionamento = 0; 
+if(65 >= Tempo)
+{
+    PrecoFinalEstacionamento=20;
+    
+}
+else if (veiculo=="P")
+{
+    PrecoFinalEstacionamento=Convert.ToDecimal(20+10*Math.Round((Tempo-60)/60));
+    
+}
+else if (veiculo=="G")
+{
+    PrecoFinalEstacionamento=Convert.ToDecimal(20*Math.Round(Tempo/60));
+    
+}
+PrecoManobrista = PrecoFinalEstacionamento*PrecoManobrista;
+Console.WriteLine($"\nEstacionamento..: {PrecoFinalEstacionamento,14:C}");
+Console.WriteLine($"Valet...........: {PrecoManobrista,14:C}");
+Console.WriteLine($"Lavagem.........: {PrecoLavagem,14:C}");
+Console.WriteLine("--------------------------------");
+decimal Total = PrecoFinalEstacionamento+PrecoManobrista+Convert.ToDecimal(PrecoLavagem);
+Console.WriteLine($"Total...........: {Total,14:C}");
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # Estacionamento
-Projeto Final
+
+Este programa recebe dados de um estacionamento. Sendo pedido:
+```
+- Tamanho do veículo;
+- O tempo do veículo estacionado;
+- O serviço de manobrista;
+- O serviço de lavagem.
+```
+## Exemplo
+```
+---Estacionamento---
+
+---Tamanho do veículo---(P/G)
+g
+---Tempo---(min)
+123
+---Serviço Manobrista---(S/N)
+s
+---Serviço Lavagem---(S/N)
+s
+
+Estacionamento..:       R$ 40,00
+Valet...........:        R$ 8,00
+Lavagem.........:      R$ 100,00
+--------------------------------
+Total...........:      R$ 148,00
+```
+
+## Screenshot
+
+![](img/tela001.png)
+
+## Download
+
+Baixe o arquivo abaixo. Descompacte na pasta desejada.
+
+[🔗 Download do arquivo .zip](dist/estacionamento.zip)
+
+Execute utilizando o comando:
+
+```
+dotnet estacionamento.dll
+```
+
+## Agradecimentos
+
+- [Etec Adolpho Berezin](https://etecab.cps.sp.gov.br/)
+- [Karine Maria da Silva](https://github.com/karine901)
+- [Karina Barbosa de Almeida](https://github.com/KarinaAlmeid4)
